421f4d37321e05eceaec82a3d99124ec601c01e5
yepes <yepes@10.160.2.4>
2026-01-21 20:41:53 +0000

    login spring security/controller

--- JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/App.java
+++ JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/App.java
@@ -1,25 +1,33 @@
 package lp.JavaFxClient_Equipa07_rec;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- */
+
 public class App extends Application{
+	
+	private static ConfigurableApplicationContext context;
+
+	
+	@Override
+    public void init() {
+        context = new SpringApplicationBuilder(SpringBootApp.class)
+        		.run();
+    }
+	
 	@Override
 	 public void start(Stage stage) throws Exception {
-	 FXMLLoader loader = new FXMLLoader(getClass().getResource("/main-view.fxml"));
+	 FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
+	 loader.setControllerFactory(context::getBean); 
+	 
 	 Scene scene = new Scene(loader.load());
-	 
-	 stage.setTitle("School API Client");
+	 stage.setTitle("Login");
 	 stage.setScene(scene);
-
-	 stage.setWidth(500); 
-	 stage.setHeight(400); 
-	 
 	 stage.show();
 	 }
 	 public static void main(String[] args) {
