6a12eddfa1c4b33775ac66b55737f595c9c5cddf
Manu <yepesmoramanu@gmail.com>
2026-01-21 15:13:35 +0000

    Add files via upload

---
+++ JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/App.java
@@ -0,0 +1,10 @@
+package lp.JavaFxClient_Equipa07_rec;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
