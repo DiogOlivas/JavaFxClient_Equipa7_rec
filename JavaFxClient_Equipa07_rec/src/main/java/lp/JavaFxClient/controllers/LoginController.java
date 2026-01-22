008415c12b54c2e0268e762e8ab12152c774d68f
yepes <yepes@10.160.2.4>
2026-01-22 20:30:36 +0000

    Login and authentication :b

--- JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient/controllers/LoginController.java
+++ JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient/controllers/LoginController.java
@@ -8,14 +8,50 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import lp.JavaFxClient_Equipa07_rec.AuthClient;
 
 
 public class LoginController {
 
     @FXML
-    private TextField txtUser;
+    private TextField txt_user;
 
     @FXML
-    private PasswordField txtPass;
+    private PasswordField txt_pass;
 
-}
+    @FXML
+    private void onLogin() {
+    	 String username = txt_user.getText();
+    	    String password = txt_pass.getText();
+
+    	    boolean success = AuthClient.login(username, password);
+
+    	    if (success) {
+    	        openMenu();
+    	    } else {
+    	        Alert alert = new Alert(Alert.AlertType.ERROR);
+    	        alert.setHeaderText("Invalid login");
+    	        alert.setContentText("Username or password is incorrect");
+    	        alert.show();
+    	    }
+    }
+    
+    private void openMenu() {
+    	try {
+    		 FXMLLoader loader = new
+    	    FXMLLoader(getClass().getResource("/menu.fxml"));
+    		 Parent root = loader.load();
+    		 
+    		 Stage stage = new Stage();
+    		 stage.setTitle("Main menu");
+    		 stage.setScene(new Scene(root));
+    		 stage.show();
+
+    		 Stage loginWindow = (Stage) txt_user.getScene().getWindow();
+    		 loginWindow.close();
+    		 
+    		 } catch (Exception e) {
+    		 e.printStackTrace();
+    		 }
+    		 }
+    }
---
+++ JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/AuthClient.java
@@ -0,0 +1,37 @@
+package lp.JavaFxClient_Equipa07_rec;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class AuthClient {
+    public static boolean login(String username, String password) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            String json = String.format("""
+                {
+                    "username": "%s",
+                    "password": "%s"
+                }
+            """, username, password);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/users/login"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+
+            HttpResponse<Void> response =
+                    client.send(request, HttpResponse.BodyHandlers.discarding());
+
+            return response.statusCode() == 200; //OK
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
