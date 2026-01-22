643e9c22103272278dad6c2ac7fb990b650f44d7
yepes <yepes@10.160.2.4>
2026-01-22 17:25:31 +0000

    Fix

--- JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/App.java
+++ JavaFxClient_Equipa07_rec/src/main/java/lp/JavaFxClient_Equipa07_rec/App.java
@@ -1,32 +1,18 @@
 package lp.JavaFxClient_Equipa07_rec;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class App extends Application{
-	
-	private static ConfigurableApplicationContext context;
-
-	
-	@Override
-    public void init() {
-        context = new SpringApplicationBuilder(SpringBootApp.class)
-        		.run();
-    }
-	
-	@Override
+public class App extends Application {
+	 @Override
 	 public void start(Stage stage) throws Exception {
-	 FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
-	 loader.setControllerFactory(context::getBean); 
-	 
-	 Scene scene = new Scene(loader.load());
-	 stage.setTitle("Login");
+	 Parent root =
+	 FXMLLoader.load(getClass().getResource("/login.fxml"));
+	 Scene scene = new Scene(root);
 	 stage.setScene(scene);
 	 stage.show();
 	 }
